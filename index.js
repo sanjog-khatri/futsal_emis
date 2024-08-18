e0dacb25336d29b79123986fdc12cc89297f70cd
sanjog-khatri <khatrisanjog6@gmail.com>
2024-08-18 23:30:44 +0545

    role insertion

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express');
-const app = express();
-
 const bodyparser = require('body-parser');
 const cors = require('cors');
-const AuthRouter = require('./Routes/AuthRouter');
+const roleRouter = require('./Routes/rolerouter'); 
+const authRouter = require('./Routes/AuthRouter');
 
 require('dotenv').config();
 require('./Models/db');
 
+const app = express();
 const PORT = 8080;
-
-app.get('/ping', (req, res) => {
-    res.send('PONG');
-})
 
 app.use(bodyparser.json());
 app.use(cors());
-app.use('/auth', AuthRouter);
+
+app.use('/role', roleRouter);
+app.use('/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
-})
+});
